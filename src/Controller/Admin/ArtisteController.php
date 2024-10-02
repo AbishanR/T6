b4ada2a6261312bbf7a3d9db693146f5f7e6c573
RAJENDRAN <abishanr30@gmail.com>
2024-10-02 17:08:51 +0200

    video1345

---
+++ src/Controller/Admin/ArtisteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArtisteController extends AbstractController
+{
+    #[Route('/admin/artistes', name: 'admin_app_artistes', methods :'GET')]
+    public function listeArtistes(ArtisteRepository $repo)
+    {
+        $artistes=$paginator->paginate(
+        $repo->listeArtistesComplete(),
+        $request->query->getInt('page', 1), /*page number*/
+        8 /*limit per page*/
+        );
+        return $this->render('artiste/listeArtistes.html.twig', [
+            'lesArtistes' => $artistes
+        ]);
+    }
+}
